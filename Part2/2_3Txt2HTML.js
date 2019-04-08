422b879a74ee3d3a7a06ad62de8b76b883c90efd
Jingjie Chen <jingjiechen96@gmail.com>
2019-04-08 17:01:03 -0400

    Delete 2_3Txt2HTML.js

--- Part2/2_3Txt2HTML.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');
-
-fs.readFile('hello.txt', 'utf-8', (err, data)=> {
-    fs.writeFile('hello.html', '<!DOCTYPE html><html><body><h3>'+ data + "</h3></body></html>", (err)=> {
-        console.log("hello.txt is online as hello.html");
-    });
-});
